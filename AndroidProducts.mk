25a8feb1203cd8daf126dcc16fcb6fd379905e6c
Ruesti <uphone@gmx.de>
2025-06-17 22:12:16 +0200

    test
    Change-Id: Ie95254f05c7ddab80b71b7e9a9f06bd2e7a8dfb5

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     qs8m_mq00:$(LOCAL_DIR)/qs8m_mq00.mk
 
-
 COMMON_LUNCH_CHOICES := \
-    qs8m_mq00-user \
-    qs8m_mq00-userdebug \
-    qs8m_mq00-eng
+    qs8m_mq00-ap2a-user \
+    qs8m_mq00-ap2a-userdebug \
+    qs8m_mq00-ap2a-eng
+
--- qs8m_mq00.mk
+++ qs8m_mq00.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/rideos/qs8m_mq00/device.mk)
 
-PRODUCT_NAME := qs8m_mq00
+PRODUCT_NAME := qs8m_mq00-ap2a
 PRODUCT_DEVICE := qs8m_mq00
 PRODUCT_BRAND := RideOS
 PRODUCT_MODEL := QS8M Reference
